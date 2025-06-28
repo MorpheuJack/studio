cf476a8bf2ba15e74087ca07d2f018718dd9fce6
Bubble Growth <bubbleg.acesso@gmail.com>
2025-06-28 01:51:16 +0000

    no mobile, tire um pouco do fundo abaixo do titulo e autor

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -50,7 +50,7 @@ export default function BlogPostPage() {
                     </Link>
                 </Button>
             </div>
-            <div className="relative z-10 flex h-full flex-col items-center justify-center text-white md:items-start md:justify-end md:pb-24">
+            <div className="relative z-10 flex h-full flex-col items-center justify-end pb-40 text-white md:items-start md:justify-end md:pb-24">
                 <div className="max-w-3xl text-center md:text-left">
                     <h1
                         className="font-headline text-4xl font-bold tracking-tight sm:text-5xl lg:text-6xl"
