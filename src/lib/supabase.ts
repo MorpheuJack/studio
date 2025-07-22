3738197a65649cc10c6b57df94a6688074ec35d9
Bubble Growth <bubbleg.acesso@gmail.com>
2025-07-22 22:49:18 +0000

    o login nao esta funcionando

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -2,7 +2,11 @@
 
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const supabaseUrl = 'https://vvvlcyoexbsihppkcyzp.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ2dmxjeW9leGJzaWhwcGtjenpwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTk4NTMwMTAsImV4cCI6MjAzNTQyOTAxMH0.2Yq3eTqte33sJ38ENu2oZJ1n-bQ26sS4a5vL5yKUyQs'
+
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error('Supabase URL and anon key are required.');
+}
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
